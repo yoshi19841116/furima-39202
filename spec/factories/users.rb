fcc85ca080e520fb926114ceedcfd72b5f1acd54
yoshi19841116 <kapibarasan_fit@icloud.com>
2023-08-21 22:24:12 +0900

    Rubocopを実行

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,15 +2,15 @@ FactoryBot.define do
   factory :user do
     transient do
       person { Gimei.name }
-   end
-    name                  {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    first_name            {person.first.kanji}
-    last_name             {person.last.kanji}
-    first_name_kana       {person.first.katakana}
-    last_name_kana        {person.last.katakana}
-    birth_date            {Faker::Date.birthday(min_age: 18, max_age: 65)}
+    end
+    name                  { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    first_name            { person.first.kanji }
+    last_name             { person.last.kanji }
+    first_name_kana       { person.first.katakana }
+    last_name_kana        { person.last.katakana }
+    birth_date            { Faker::Date.birthday(min_age: 18, max_age: 65) }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,9 +6,8 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
-
     it 'nameが空では登録できない' do
-      @user.name = ''  
+      @user.name = ''
       @user.valid?
       expect(@user.errors.full_messages).to include("Name can't be blank")
     end
@@ -36,22 +35,22 @@ RSpec.describe User, type: :model do
     end
 
     it 'emailが空では登録できない' do
-      @user.email = ''  
+      @user.email = ''
       @user.valid?
       expect(@user.errors.full_messages).to include("Email can't be blank")
     end
 
     it 'メールアドレスが一意性であること' do
       @user.save
-        another_user = FactoryBot.build(:user, email: @user.email)
-        another_user.valid?
-        expect(another_user.errors.full_messages).to include('Email has already been taken')
+      another_user = FactoryBot.build(:user, email: @user.email)
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include('Email has already been taken')
     end
 
     it 'メールアドレスは、@を含む必要があること' do
       @user.email = 'testmail'
-        @user.valid?
-        expect(@user.errors.full_messages).to include('Email is invalid')
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Email is invalid')
     end
 
     it 'パスワードが必須であること' do
@@ -62,16 +61,16 @@ RSpec.describe User, type: :model do
 
     it 'パスワードは、6文字以上での入力が必須であること' do
       @user.password = '12345'
-        @user.password_confirmation = '12345'
-        @user.valid?
-        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      @user.password_confirmation = '12345'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
     end
 
     it 'パスワードとパスワード（確認）は、値の一致が必須であること' do
       @user.password = '123456'
-        @user.password_confirmation = '1234567'
-        @user.valid?
-        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      @user.password_confirmation = '1234567'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
   end
 end
