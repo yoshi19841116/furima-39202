1f59d3c57c6124646a0f2025fc574b42e065d985
yoshi19841116 <kapibarasan_fit@icloud.com>
2023-08-24 04:49:10 +0900

    アソシエーションの追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,10 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee_burden
+  belongs_to :prefecture
+  belongs_to :day_to_ship
   belongs_to :user
   has_one_attached :image
-
-  
 end
