ec70effe99ed424fbe8ccf844690ff0d1e7b60c2
yoshi19841116 <kapibarasan_fit@icloud.com>
2023-08-24 00:52:14 +0900

    商品テーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_20_172359) do
+ActiveRecord::Schema.define(version: 2023_08_23_153644) do
+
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "product_name", null: false
+    t.text "description", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "shipping_fee_burden_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "day_to_ship_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -42,4 +56,5 @@ ActiveRecord::Schema.define(version: 2023_08_20_172359) do
     t.index ["reset_password_token"], name: "index_views_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "users"
 end
