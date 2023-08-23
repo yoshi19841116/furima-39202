5266607ae0f59cba41e0904bd4a072e2ff4a3aaa
yoshi19841116 <kapibarasan_fit@icloud.com>
2023-08-24 05:34:01 +0900

    バリテーションの設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,14 @@ class Product < ApplicationRecord
   belongs_to :day_to_ship
   belongs_to :user
   has_one_attached :image
+
+  validates :product_name, :string, presence: true
+  validates :description, :text, presence: true
+  validates :price, :integer, presence: true
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :shipping_fee_burden_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :day_to_ship_id, numericality: { other_than: 1, message: "can't be blank"} 
 end
