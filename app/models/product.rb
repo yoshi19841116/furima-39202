dbe81a92e0ef9d50fa1d12bc8437166f9fcedd84
yoshi19841116 <kapibarasan_fit@icloud.com>
2023-08-25 02:01:14 +0900

    バリテーションの修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,9 +8,9 @@ class Product < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :product_name, presence: true
-  validates :description, presence: true
-  validates :price, presence: true
+  validates :product_name, presence: true, length: { maximum: 40 }
+  validates :description, presence: true, length: { maximum: 1000 }
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1, message: "can't be blank"}
