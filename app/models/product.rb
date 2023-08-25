ad7e123d8fd66e58bffb09ad900639e74a0584e0
yoshi19841116 <kapibarasan_fit@icloud.com>
2023-08-25 22:24:37 +0900

    画像のバリデーション追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,6 +8,7 @@ class Product < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  validates :image, presence: true
   validates :product_name, presence: true, length: { maximum: 40 }
   validates :description, presence: true, length: { maximum: 1000 }
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
