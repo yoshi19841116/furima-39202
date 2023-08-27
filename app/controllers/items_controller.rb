14396380b635b7c2655b3f065276ad31106e2af0
yoshi19841116 <kapibarasan_fit@icloud.com>
2023-08-27 17:59:24 +0900

    商品の編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_product, only: [:edit, :update]
 
   def index
     @products = Product.order(created_at: :desc)
@@ -30,7 +31,27 @@ class ItemsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+    if current_user != @product.user
+      redirect_to root_path, alert: "出品者以外は編集できません。"
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to item_path(@product), notice: "商品情報が更新されました。"
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def set_product
+    product = Product.find(params[:id])
+  end
 
   def product_params
     params.require(:product).permit(:product_name, :description, :category_id, :condition_id, :shipping_fee_burden_id,
