f62594a4178c85c713104ce73b2742c57a65a617
yoshi19841116 <kapibarasan_fit@icloud.com>
2023-09-01 09:59:06 +0900

    公開鍵と秘密鍵の環境変数の設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -42,11 +42,11 @@ class OrdersController < ApplicationController
   end
   
   def pay_item
-    Payjp.api_key = "  "  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-      amount: order_params[:price],  # 商品の値段
-      card: order_params[:token],    # カードトークン
-      currency: 'jpy'                 # 通貨の種類（日本円）
+      amount: order_params[:price],
+      card: order_params[:token],
+      currency: 'jpy'
     )
   end
 end
